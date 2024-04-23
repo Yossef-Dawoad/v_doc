22efa3626aa4ac157a5778ad4092292fae0e56d3
Yossef-Dawoad <yossefdawoad15@gmail.com>
2024-04-23 20:19:26 +0200

    feat:setup flavors for android

--- lib/main_development.dart
+++ lib/main_development.dart
@@ -1,14 +1,24 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:logging/logging.dart';
 import 'package:v_doc/doc_app.dart';
 
 import 'core/di/dependency_inject.dart';
 import 'core/routes/app_router.dart';
 
-void main() {
+Future<void> main() async {
   initializeDependence();
   _setupLogging();
+  // wait for window size to be initialized
+  // fix font loading isuess with .sp sizing in release Mode
+  await ScreenUtil.ensureScreenSize();
+  SystemChrome.setSystemUIOverlayStyle(
+    const SystemUiOverlayStyle(
+      systemNavigationBarColor: Colors.amber,
+    ),
+  );
   runApp(DocApp(router: AppRouter()));
 }
 
