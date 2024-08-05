c66e870a53e9a64c48c0ecd61daf27195b73cf12
Yossef-Dawoad <yossefdawoad15@gmail.com>
2024-08-05 23:45:01 +0300

    complete home banner UI

--- lib/features/home/view/home_screen.dart
+++ lib/features/home/view/home_screen.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+import 'package:flutter/widgets.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:v_doc/features/home/widget/welcome_app_bar.dart';
 
 class HomeScreen extends StatelessWidget {
@@ -16,9 +19,73 @@ class HomeScreen extends StatelessWidget {
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
               WelcomeHomeAppBar(),
+              FindDoctorPrimaryBanner(),
             ],
           ),
         ),
+      ),
+    );
+  }
+}
+
+class FindDoctorPrimaryBanner extends StatelessWidget {
+  const FindDoctorPrimaryBanner({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 195.h,
+      child: Stack(
+        alignment: Alignment.bottomCenter,
+        children: [
+          Container(
+            width: double.infinity,
+            height: 165.h,
+            padding: EdgeInsets.symmetric(horizontal: 16.w, vertical: 16.h),
+            decoration: const BoxDecoration(
+              borderRadius: BorderRadius.all(Radius.circular(24.0)),
+              image: DecorationImage(
+                image: AssetImage('assets/images/blue_bg_banner.png'),
+                fit: BoxFit.cover,
+              ),
+            ),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  'Book and\nschedule with\nnearest doctor',
+                  style: TextStyle(
+                    fontSize: 18.sp,
+                    fontWeight: FontWeight.w700,
+                    color: Colors.white,
+                  ),
+                ),
+                SizedBox(height: 16.h),
+                Expanded(
+                  child: ElevatedButton(
+                    onPressed: () {},
+                    style: ElevatedButton.styleFrom(
+                      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(40)),
+                      textStyle: TextStyle(
+                        fontSize: 14.sp,
+                        fontWeight: FontWeight.w700,
+                        color: const Color(0xff227cff),
+                      ),
+                    ),
+                    child: const Text('Find Nearby'),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Positioned(
+            right: 8.w,
+            child: Image.asset(
+              'assets/images/female_doc.png',
+              height: 200.h,
+            ),
+          )
+        ],
       ),
     );
   }
